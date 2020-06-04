e54b92a0cac54308149338ef389ecd9b2d6df31b
Lucas <lsg.comunicacoes@gmail.com>
2020-06-04 17:26:21 -0400

    Mudança do Gauge

--- public/js/realtime.js
+++ public/js/realtime.js
@@ -1,19 +1,9 @@
+//direcionamento da msg do device para a pagina web
+
 const socket = io('http://localhost:3000/');
+socket.on("chegou", (msg)=>{
+    console.log('msg',msg.payload_field);
+});
 
 
-function markerAction(event) {
 
-    $("#modalDevice").modal();
-   
-    socket.on("chegou", (msg)=>{
-        console.log('msg',msg);
-        $("#gaugeGas").attr("data-value",msg.payload_field.payload_gas);  
-    });
-    //animação para node 1 interagir com o click
-    if (this.getAnimation() !== null) {//condição seria payload diferente de vazio
-        this.setAnimation(null);
-    } else {
-      this.setAnimation(google.maps.Animation.BOUNCE);
-    }
-
-}
--- public/js/script.js
+++ public/js/script.js
@@ -35,11 +35,47 @@ function initMap() {
 
     //debugger
     
-    node_ufam_01.addListener('click', markerAction);//evento seria uplink
-    node_ufam_02.addListener('click', markerAction);
-    node_ufam_03.addListener('click', markerAction);
-    //document.getElementById("content").addEventListener('click', ()=>document.getElementById("content").style.display = "none");//() => mesma coisa q uma função
+    node_ufam_01.addListener('click', toggleBounce);//evento seria uplink
+    node_ufam_02.addListener('click', toggleBounce);
+    node_ufam_03.addListener('click', toggleBounce);
+    document.querySelector(".whiteWindow").addEventListener('click', ()=>document.querySelector(".whiteWindow").style.display = "none");//() => mesma coisa q uma função
     
-  }
+    //let infowindow = new google.maps.InfoWindow({content: "No data"});
+    const gaugeElement1 = document.querySelector(".gauge1");
+    const gaugeElement2 = document.querySelector(".gauge2");
+    
+    function setGaugeValue(gauge1,gauge2,value1,value2){
+        //document.getElementById("pop-up").style.display = "block";
+        if(value1 < 0 || value1 > 1){
+            return;
+        }
+        if(value2 < 0 || value2 > 1){
+            return;
+        }
+                        
+        gauge1.querySelector(".gauge_fill1").style.transform = `rotate(${value1/2}turn)`;
+        gauge1.querySelector(".gauge_cover1").textContent = `${Math.round(value1 * 100)}ppm`;
+        
+        gauge2.querySelector(".gauge_fill2").style.transform = `rotate(${value2/2}turn)`;
+        gauge2.querySelector(".gauge_cover2").textContent = `${Math.round(value2 * 100)}ppm`;
+        }
+    
+    function toggleBounce(event) {
+        //setGaugeValue(gaugeElement1,gaugeElement2,0.9,0.36);
+        document.querySelector(".whiteWindow").style.display = "block"
+      //infowindow.open(map,this)//mostrar a janela do marcador clicado
+        //animação para node 1 interagir com o click
+        if (this.getAnimation() !== null) {//condição seria payload diferente de vazio
+            this.setAnimation(null);
+        } else {
+          this.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    
+    }
+}
+
+    
+
+
 
   
