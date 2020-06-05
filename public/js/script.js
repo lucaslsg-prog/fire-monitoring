a9382a586c078189091de7fbe8c2fa755ac1c42a
George Bentes <george.bentes@gmail.com>
2020-06-04 22:09:57 -0300

    Add gauge inside a small bootstrap modal

--- public/js/script.js
+++ public/js/script.js
@@ -27,55 +27,19 @@ function initMap() {
       mapOptions
     );
     
-    
-    const gateway = new google.maps.Marker({ position: position_gateway, map,label:'LoRa Gateway'});
     const node_ufam_01 = new google.maps.Marker({ position: position_node1, map,label:'node-ufam-01',animation: google.maps.Animation.DROP});
     const node_ufam_02 = new google.maps.Marker({ position: position_node2, map,label:'ufam-modem-01',animation: google.maps.Animation.DROP});
     const node_ufam_03 = new google.maps.Marker({ position :position_node3, map,label:'ufam-modem-02',animation: google.maps.Animation.DROP});
-
-    //debugger
     
     node_ufam_01.addListener('click', toggleBounce);//evento seria uplink
     node_ufam_02.addListener('click', toggleBounce);
     node_ufam_03.addListener('click', toggleBounce);
-    document.querySelector(".whiteWindow").addEventListener('click', ()=>document.querySelector(".whiteWindow").style.display = "none");//() => mesma coisa q uma função
     
-    //let infowindow = new google.maps.InfoWindow({content: "No data"});
-    const gaugeElement1 = document.querySelector(".gauge1");
-    const gaugeElement2 = document.querySelector(".gauge2");
     
-    function setGaugeValue(gauge1,gauge2,value1,value2){
-        //document.getElementById("pop-up").style.display = "block";
-        if(value1 < 0 || value1 > 1){
-            return;
-        }
-        if(value2 < 0 || value2 > 1){
-            return;
-        }
-                        
-        gauge1.querySelector(".gauge_fill1").style.transform = `rotate(${value1/2}turn)`;
-        gauge1.querySelector(".gauge_cover1").textContent = `${Math.round(value1 * 100)}ppm`;
-        
-        gauge2.querySelector(".gauge_fill2").style.transform = `rotate(${value2/2}turn)`;
-        gauge2.querySelector(".gauge_cover2").textContent = `${Math.round(value2 * 100)}ppm`;
-        }
-    
-    function toggleBounce(event) {
-        //setGaugeValue(gaugeElement1,gaugeElement2,0.9,0.36);
-        document.querySelector(".whiteWindow").style.display = "block"
-      //infowindow.open(map,this)//mostrar a janela do marcador clicado
-        //animação para node 1 interagir com o click
-        if (this.getAnimation() !== null) {//condição seria payload diferente de vazio
-            this.setAnimation(null);
-        } else {
-          this.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    
-    }
+    $('#modalDevice').on('hidden.bs.modal', () => {
+        node_ufam_01.setAnimation(null);
+        node_ufam_02.setAnimation(null);
+        node_ufam_03.setAnimation(null);
+    });
 }
 
-    
-
-
-
-  
